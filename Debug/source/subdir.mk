545370725155bc1544d9c11c36989eea8a1e67e3
jzapataE <jzapata@cinvestav.mx>
2024-08-01 16:57:39 -0600

    First Commit

---
+++ Debug/source/subdir.mk
@@ -0,0 +1,34 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../source/main.c \
+../source/semihost_hardfault.c 
+
+C_DEPS += \
+./source/main.d \
+./source/semihost_hardfault.d 
+
+OBJS += \
+./source/main.o \
+./source/semihost_hardfault.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+source/%.o: ../source/%.c source/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DCPU_MK64FN1M0VLL12 -DCPU_MK64FN1M0VLL12_cm4 -DSDK_OS_BAREMETAL -DSDK_DEBUGCONSOLE=1 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -DSERIAL_PORT_TYPE_UART=1 -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I"D:\DM_TAE\SDK\Testing\board" -I"D:\DM_TAE\SDK\Testing\source" -I"D:\DM_TAE\SDK\Testing\utilities" -I"D:\DM_TAE\SDK\Testing\drivers" -I"D:\DM_TAE\SDK\Testing\device" -I"D:\DM_TAE\SDK\Testing\component\serial_manager" -I"D:\DM_TAE\SDK\Testing\component\lists" -I"D:\DM_TAE\SDK\Testing\CMSIS" -I"D:\DM_TAE\SDK\Testing\component\uart" -O0 -fno-common -g3 -gdwarf-4 -Wall -c -ffunction-sections -fdata-sections -ffreestanding -fno-builtin -fmerge-constants -fmacro-prefix-map="$(<D)/"= -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-source
+
+clean-source:
+	-$(RM) ./source/main.d ./source/main.o ./source/semihost_hardfault.d ./source/semihost_hardfault.o
+
+.PHONY: clean-source
+
---
+++ Debug/startup/subdir.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../startup/startup_mk64f12.c 
+
+C_DEPS += \
+./startup/startup_mk64f12.d 
+
+OBJS += \
+./startup/startup_mk64f12.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+startup/%.o: ../startup/%.c startup/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DCPU_MK64FN1M0VLL12 -DCPU_MK64FN1M0VLL12_cm4 -DSDK_OS_BAREMETAL -DSDK_DEBUGCONSOLE=1 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -DSERIAL_PORT_TYPE_UART=1 -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I"D:\DM_TAE\SDK\Testing\board" -I"D:\DM_TAE\SDK\Testing\source" -I"D:\DM_TAE\SDK\Testing\utilities" -I"D:\DM_TAE\SDK\Testing\drivers" -I"D:\DM_TAE\SDK\Testing\device" -I"D:\DM_TAE\SDK\Testing\component\serial_manager" -I"D:\DM_TAE\SDK\Testing\component\lists" -I"D:\DM_TAE\SDK\Testing\CMSIS" -I"D:\DM_TAE\SDK\Testing\component\uart" -O0 -fno-common -g3 -gdwarf-4 -Wall -c -ffunction-sections -fdata-sections -ffreestanding -fno-builtin -fmerge-constants -fmacro-prefix-map="$(<D)/"= -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-startup
+
+clean-startup:
+	-$(RM) ./startup/startup_mk64f12.d ./startup/startup_mk64f12.o
+
+.PHONY: clean-startup
+
