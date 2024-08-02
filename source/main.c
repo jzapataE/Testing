7acd1c129ff0eaee5cac880bc4b3533195e80c4c
jzapataE <jzapata@cinvestav.mx>
2024-08-01 20:07:09 -0600

    Comments v1

--- source/main.c
+++ source/main.c
@@ -1,27 +1,32 @@
+
 #include <stdio.h>
 #include <stdint.h>
 #include "MK64F12.h"
 
+
 int main(void) {
 
-	SIM->SCGC5 = 0x400;
-
-	PORTB->PCR[22] = 0x00000100;
-	GPIOB->PDOR = 0x00400000;
-	GPIOB->PDDR = 0x00400000;
+	// Configuration, internal registers
+	SIM->SCGC5     = 0x00000400; // Clock Gating Control, Clock enabled on PORT B
+	PORTB->PCR[22] = 0x00000100; // Pin Control Register, MUX to GPIO (001)
+	GPIOB->PDOR    = 0x00400000;
+	GPIOB->PDDR    = 0x00400000;
 
 	while(1) {
+
 		GPIOB->PDOR = 0;
 
 		printf("RED LED ON\n");
 
+
 		GPIOB->PDOR = 0x00400000;
+
 		GPIOB->PDOR = 0x00000000;
+
 		GPIOB->PDOR = 0x00200000;
 
 		printf("RED LED OFF\n");
 
 	}
-
 	return 0;
 }
